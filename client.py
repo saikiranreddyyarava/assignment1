5b6e293ebc1b2a9e48c80c16140ca18e8e895147
saikiranreddyyarava <kiranreddyyarava@gmail.com>
2019-10-20 20:50:51 +0200

    new commit

--- client.py
+++ client.py
@@ -1,58 +1,48 @@
+#!/usr/bin/python
 
+import socket,sys,select,string
 
-import sys, socket, select
+socketObject = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-def chat_client():
-    if (len(sys.argv) < 3):
-    	print("ENTER: python chatclient.py hostname:port nickname")
-    	sys.exit()
-    arg = sys.argv[1].split(':')
-    host =arg[0]
-    port = int(arg[1])
-    nickname = sys.argv[2]
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(2)
+nrArg = len(sys.argv)
+if (nrArg < 3 and nrArg > 4):
+    print ("Wrong Input")
+    print ("please ENTER: python filename hostname:port USER")
+    sys.exit()
 
+array = sys.argv[1].split(':')
+host = array[0]
+port = int(array[1])
+user = sys.argv[2]
+socketObject.settimeout(10)
+socketObject.connect((host,port))
+print("Connection established to server")
+nick = 'NICK '+ user
+socketObject.send(nick)
+string =''
 
-    try :
-        s.connect((host, port))
-    except :
-        print 'Unable to connect'
-        sys.exit()
-
-    print 'Connected to remote host. You can start sending messages'
-    
-    nick = 'NICK ' + nickname
-    s.send(nick)
-    sys.stdout.write('[Me] '); sys.stdout.flush()
-
-    while 1:
-        socket_list = [sys.stdin, s]
-
-
-        read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
-
-        for sock in read_sockets:
-            if sock == s:
-
-                data = sock.recv(4096)
-                if not data :
-                    print '\nDisconnected from chat server'
-                    sys.exit()
-                else :
-
-                    sys.stdout.write(data)
-                    sys.stdout.write('[Me] '); sys.stdout.flush()
-
-            else :
-
-                msg = sys.stdin.readline()
-                msgsnd = 'MSG ' + msg
-		s.send(msgsnd.encode('utf-8'))
-                sys.stdout.write('[Me] '); sys.stdout.flush()
-
-if __name__ == "__main__":
-
-    sys.exit(chat_client())
-
-
+while True :
+    sockList = [sys.stdin,socketObject]
+    readList,writeList,errorList = select.select(sockList,[], [])
+    for i in readList:
+        if i == socketObject:
+            rcvdMsg = i.recv(1024)
+            if not rcvdMsg:
+                print("DISCONNECTED")
+                sys.exit()
+            else:
+                if rcvdMsg != 'MSG '+user+' '+string :
+                    sys.stdout.write(rcvdMsg.strip('MSG '))
+                    sys.stdout.flush()
+                else:
+                    continue
+        else:
+            string = ''
+            msg = sys.stdin.readline()
+            string += msg
+            fullmsg ='MSG ' + msg
+            socketObject.send(fullmsg.encode('utf-8'))
+            sys.stdout.write("You : ")
+            sys.stdout.write(msg)
+            sys.stdout.flush()
+socketObject.close()
