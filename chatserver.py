29392635c745cc20eaea67d029f8435659f38677
saikiranreddyyarava <kiranreddyyarava@gmail.com>
2020-05-24 20:55:46 +0200

    Added basic chat server

--- chatserver.py
+++ chatserver.py
@@ -1,80 +1,89 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import socket,sys, re
-from threading import Thread
+import sys, socket, select
 
-socketObject = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+HOST = ''
+SOCKET_LIST = []
+RECV_BUFFER = 4096
+PORT = 9000
 
-nrArg = len(sys.argv)
-if (nrArg < 2 and nrArg > 3):
-    print ("Wrong Input")
-    print ("please ENTER: python <FILENAME> localhost:<PORT> <USER>")
-    sys.exit()
-else:
-    array = sys.argv[1].split(':')
-    host = array[0]
-    port = int(array[1])
-    clients = {}
-    clientList = []
-    socketObject.bind((host,port))
+def chat_server():
 
-# Function to show the client connection
-# Shows the chatstatus to clients
-def clientAcceptance() :
-    while True :
-        ip,addr = socketObject.accept()
-        print("%s:%s has connected" % addr)
-        ip.send("HELLO 1 \n")
-        clientList.append(ip)
-        Thread(target= handlingClients, args=(ip,addr)).start()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(10)
 
-# Function to validate the message size
-# Client connection status
-def handlingClients(ip,addr) :
+    # add server socket object to the list of readable connections
+    SOCKET_LIST.append(server_socket)
 
-    length = ip.recv(1024)
-    if len(length)<13 and re.match("^[A-Za-z0-9\_]+$",length) is not None:
-        ip.send("OK \n")
-        msg = "%s has joined the chat \n"%length
-        broadcast(msg,ip)
-        clients[ip] = length
-    else :
-        ip.send("ERROR: Nickname is not valid \n")
-        name = 'unknown'
-        ip.close()
-        #del clients[ip]
+    print("Chat server started on {}:{}".format(HOST,PORT))
 
+    while 1:
 
-    while True :
-        m = ip.recv(1024).decode('utf-8')
-        msg = m.strip("MSG ")
-        if not msg:
-            ip.close()
-            del clients[ip]
-            left = "%s has left the chat \n"% length
-            print ("%s:%s has disconnected." % addr)
-            broadcast(left,ip)
-            break
-        else :
-            if len(msg) > 255 and re.match("^[^\x00-\x7F]*$",msg) is None:
-                ip.send("ERROR: Message should be less than 255 characters \n")
-                msg_snd = "MSG "+length +" "
+        # get the list sockets which are ready to be read through select
+        # 4th arg, time_out  = 0 : poll and never block
+        ready_to_read,ready_to_write,in_error = select.select(SOCKET_LIST,[],[],0)
+
+        for sock in ready_to_read:
+            # a new connection request recieved
+            if sock == server_socket:
+                sockfd, addr = server_socket.accept()
+                SOCKET_LIST.append(sockfd)
+                print("Client %s:%s connected" % addr)
+
+                broadcast(server_socket, sockfd, "\n[%s:%s] entered our chatting room\n" % addr)
+
+            # a message from a client, not a new connection
             else:
-                msg_snd = "MSG "+length +" "+ msg
-            broadcast(msg_snd,ip)
+                # process data recieved from client,
+                try:
+                    # receiving data from the socket.
+                    data = sock.recv(RECV_BUFFER).decode()
+                    print("\ndebug: msg from client {}".format(data))
+                    if data:
+                        # there is something in the socket
+                        broadcast(server_socket, sock, "\r" + '[' + str(sock.getpeername()) + '] ' + data)
+                    else:
+                        # remove the socket that's broken
+                        if sock in SOCKET_LIST:
+                            SOCKET_LIST.remove(sock)
 
-def broadcast(msg, clientConnection):
-    for i in clientList:
-        if (i != socketObject) and (i != clientConnection) :
+                        # at this stage, no data means probably the connection has been broken
+                        broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
+
+                # exception
+                except:
+                    broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
+                    continue
+
+    server_socket.close()
+
+# broadcast chat messages to all connected clients
+def broadcast(server_socket, sock, message):
+    # print("debug: broadcasting messages......")
+    # print("debug: socket_list: {}".format(SOCKET_LIST))
+    for socket in SOCKET_LIST:
+        # print("debug: \nsocket: {}\nserver_socket: {}\nsock: {}\nmsg: {}".format(socket,server_socket,sock,message))
+        # send the message only to peer
+        if socket != server_socket and socket != sock:
+            # print("debug: broadcast  {}".format(message))
             try:
-                i.send(msg.encode('utf-8'))
-            except:
-                pass
+                socket.send(message.encode())
+            except :
+                # broken socket connection
+                socket.close()
+                # broken socket, remove it
+                if socket in SOCKET_LIST:
+                    SOCKET_LIST.remove(socket)
+    print("debug: broadcast complete. {} ---- {}".format(len(SOCKET_LIST),SOCKET_LIST))
 
-while True:
-    socketObject.listen(100)
-    print("Waiting for clients to connect...")
-    connection = Thread(target=clientAcceptance)
-    connection.start()
-    connection.join()
-    socketObject.close()
+if __name__ == "__main__":
+    N = len(sys.argv)
+    if N < 2:
+        print ("USAGE: ./chatserver <IP>:<PORT>")
+        sys.exit()
+    host = sys.argv[1].split(':')
+    HOST = host[0]
+    PORT = int(host[1])
+    sys.exit(chat_server())
